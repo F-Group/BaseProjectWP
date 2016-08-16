e85be54754e713ec8631df9f1a65902824c7f87a
Duy Tan <duytan1909@gmail.com>
2016-08-16 21:04:05 +0700

    update core files

---
+++ wp-content/themes/fplus/footer.php
@@ -0,0 +1,5 @@
+    </div><!-- END MAIN-->
+</div><!-- END WRAPPER-->
+<?php wp_footer(); ?>
+</body> <!--end body-->
+</html> <!--end html -->
---
+++ wp-content/themes/fplus/index.php
@@ -0,0 +1,11 @@
+<?php
+    get_header();
+?>
+<div class="blocks">
+    <section>
+
+    </section>
+</div>
+<?php
+    get_footer();
+?>
